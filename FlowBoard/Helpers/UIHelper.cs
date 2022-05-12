d1f1eacc771a84613fc6a981e2cc1904714e025d
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2022-05-13 00:54:03 +0200

    Pen Glow preview

--- FlowBoard/Helpers/UIHelper.cs
+++ FlowBoard/Helpers/UIHelper.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Input.Inking;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace FlowBoard.Helpers
@@ -24,5 +25,9 @@ namespace FlowBoard.Helpers
                 return ink.DrawAsHighlighter == true ? FluentSymbol.Highlight24 : FluentSymbol.InkingTool24;
             }
         }
+
+        public static CornerRadius TipToRadius(int index) => index == 0 ? new CornerRadius(12, 12, 12, 12) : new CornerRadius(1, 1, 1, 1);
+
+        public static double PenSizeToUISize(double size) => (0.2 * size) + 4;//(10 * Math.Log10(size + 1) + 1); //10log(x + 1) + 1 is the function for representing the size
     }
 }
